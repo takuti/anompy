724ab42e211d320b75e2f585b43fadd102604863
Takuya Kitazawa <k.takuti@gmail.com>
2016-10-15 12:50:00 +0900

    Add init core files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='anompy',
+    version='0.0.1',
+    description='A library for anomaly detection',
+    author='Takuya Kitazawa',
+    author_email='k.takuti@gmail.com',
+    license='MIT',
+    url='https://github.com/takuti/anompy',
+    packages=['anompy'],
+    install_requires=[],
+)
