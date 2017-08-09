c1aac57fdace99b41bd49c20f698d6976d990c85
Takuya Kitazawa <k.takuti@gmail.com>
2017-08-09 09:03:17 +0900

    Implement moving-average-based detector

--- anompy/detector/simple.py
+++ anompy/detector/simple.py
@@ -1,20 +1,27 @@
+from collections import deque
+
 from anompy.detector.base import BaseDetector
 
 
 class AverageDetector(BaseDetector):
 
-    def __init__(self):
+    def __init__(self, window_size=None):
+        if window_size is not None:
+            self.window_size = window_size
+            self.window = deque(maxlen=window_size)
+        else:
+            self.cnt = 0.
         self.avg = 0.
-        self.cnt = 0.
 
     def forecast(self):
         return self.avg
 
     def observe(self, x):
-        """m_{n} = ((n - 1) m_{n-1} + x_n) / n
-
-        `(n - 1) m_{n-1}` returns sum of x_1, x_2, ..., x_{n-1}
-
-        """
-        self.cnt += 1
-        self.avg = self.avg + (x - self.avg) / self.cnt
+        if hasattr(self, 'window'):  # moving average
+            self.window.append(x)
+            self.avg = sum(self.window) / self.window_size
+        else:  # simple average
+            # m_{n} = ((n - 1) m_{n-1} + x_n) / n
+            # `(n - 1) m_{n-1}` returns sum of x_1, x_2, ..., x_{n-1}
+            self.cnt += 1
+            self.avg = self.avg + (x - self.avg) / self.cnt
--- anompy/detector/tests/test_simple.py
+++ anompy/detector/tests/test_simple.py
@@ -5,7 +5,7 @@ from anompy.detector.simple import AverageDetector
 
 class AverageDetectorTestCase(TestCase):
 
-    def test(self):
+    def test_simple_average(self):
         detector = AverageDetector()
         s = 0.
 
@@ -14,3 +14,16 @@ class AverageDetectorTestCase(TestCase):
             s += x
             self.assertAlmostEqual(detector.forecast(), s / x, delta=1e-6)
             self.assertEqual(detector.is_anomaly(threshold=x), False)
+
+    def test_moving_average(self):
+        detector = AverageDetector(window_size=5)
+
+        for x in range(1, 6):
+            detector.observe(x)
+        self.assertAlmostEqual(detector.forecast(), sum(range(1, 6)) / 5, delta=1e-6)
+        self.assertEqual(detector.is_anomaly(threshold=1.), True)
+
+        for x in range(6, 11):
+            detector.observe(x)
+        self.assertAlmostEqual(detector.forecast(), sum(range(6, 11)) / 5, delta=1e-6)
+        self.assertEqual(detector.is_anomaly(threshold=6), True)
