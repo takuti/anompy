7130887e05a1da8213425ebd1f7a17d8c7420bc8
Takuya Kitazawa <k.takuti@gmail.com>
2017-08-09 09:21:37 +0900

    Implement weighted moving average

--- anompy/detector/average.py
+++ anompy/detector/average.py
@@ -5,11 +5,11 @@ from anompy.detector.base import BaseDetector
 
 class AverageDetector(BaseDetector):
 
-    def __init__(self, window_size=None):
+    def __init__(self, window_size=None, weights=None):
         self.moving_average = window_size is not None
         if self.moving_average:
-            self.window_size = window_size
             self.window = deque(maxlen=window_size)
+            self.weights = weights
         else:
             self.cnt = 0.
         self.avg = 0.
@@ -20,7 +20,10 @@ class AverageDetector(BaseDetector):
     def observe(self, x):
         if self.moving_average:  # moving average
             self.window.append(x)
-            self.avg = sum(self.window) / self.window_size
+            if self.weights is not None:
+                self.avg = sum([xi * wi for xi, wi in zip(self.window, self.weights)])
+            else:
+                self.avg = sum(self.window) / len(self.window)
         else:  # simple average
             # m_{n} = ((n - 1) m_{n-1} + x_n) / n
             # `(n - 1) m_{n-1}` returns sum of x_1, x_2, ..., x_{n-1}
--- anompy/detector/tests/test_average.py
+++ anompy/detector/tests/test_average.py
@@ -27,3 +27,11 @@ class AverageDetectorTestCase(TestCase):
             detector.observe(x)
         self.assertAlmostEqual(detector.forecast(), sum(range(6, 11)) / 5, delta=1e-6)
         self.assertEqual(detector.is_anomaly(threshold=6), True)
+
+    def test_weighted_moving_average(self):
+        detector = AverageDetector(window_size=4, weights=[0.1, 0.2, 0.3, 0.4])
+
+        for x in [3, 10, 12, 13, 12, 10, 12]:
+            detector.observe(x)
+        self.assertAlmostEqual(detector.forecast(), 11.5, delta=1e-6)
+        self.assertEqual(detector.is_anomaly(threshold=11.), True)
