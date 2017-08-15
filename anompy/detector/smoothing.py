7d214a74cb97b0c8464dd83f7478caef777e70d2
Takuya Kitazawa <k.takuti@gmail.com>
2017-08-15 13:41:47 +0900

    Implement double exponential smoothing

--- anompy/detector/smoothing.py
+++ anompy/detector/smoothing.py
@@ -18,3 +18,32 @@ class ExponentialSmoothing(BaseDetector):
             self.forecast_cache = x
         self.x_last = x
         return self.forecast_cache > self.threshold
+
+
+class DoubleExponentialSmoothing(BaseDetector):
+
+    def __init__(self, alpha=0.5, beta=0.5, threshold=0.):
+        self.alpha = alpha
+        self.beta = beta
+        self.threshold = threshold
+        self.x_last = 0.
+
+    def forecast(self):
+        self.level_cache, self.level = self.level, self.alpha * self.x_last + (1. - self.alpha) * (self.level + self.trend)
+        self.trend = self.beta * (self.level - self.level_cache) + (1. - self.beta) * self.trend
+
+        return self.level + self.trend
+
+    def observe(self, x):
+        if not hasattr(self, 'level'):
+            self.level = x
+            return False
+
+        if not hasattr(self, 'trend'):
+            self.trend = x - self.level
+            self.level_cache, self.level = self.level, x
+            self.trend = self.beta * (self.level - self.level_cache) + (1. - self.beta) * self.trend
+
+        self.x_last = x
+
+        return (self.level + self.trend) > self.threshold
--- anompy/detector/tests/test_smoothing.py
+++ anompy/detector/tests/test_smoothing.py
@@ -1,11 +1,11 @@
 from unittest import TestCase
 
-from anompy.detector.smoothing import ExponentialSmoothing
+from anompy.detector.smoothing import ExponentialSmoothing, DoubleExponentialSmoothing
 
 
 class ExponentialSmoothingTestCase(TestCase):
 
-    def test(self):
+    def test_single(self):
         detector = ExponentialSmoothing(alpha=.1)
 
         series = [3, 10, 12, 13, 12, 10, 12]
@@ -14,5 +14,20 @@ class ExponentialSmoothingTestCase(TestCase):
 
         for i in range(len(series)):
             anomaly = detector.observe(series[i])
+            self.assertTrue(anomaly)
+            self.assertAlmostEqual(detector.forecast(), expected[i], delta=eps)
+
+    def test_double(self):
+        detector = DoubleExponentialSmoothing(alpha=.9, beta=.9)
+
+        series = [3, 10, 12, 13, 12, 10, 12]
+        expected = [3, 17.0, 15.45, 14.210500000000001, 11.396044999999999, 8.183803049999998, 12.753698384500002, 13.889016464000003]
+        eps = 1e-6
+
+        for i in range(len(series)):
+            anomaly = detector.observe(series[i])
+            if i < 2:
+                continue
+
             self.assertTrue(anomaly)
             self.assertAlmostEqual(detector.forecast(), expected[i], delta=eps)
