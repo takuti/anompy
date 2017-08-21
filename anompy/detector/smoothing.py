fbc0944a42b6dc84f847b1d435125ce694aa333a
Takuya Kitazawa <k.takuti@gmail.com>
2017-08-21 21:26:59 +0900

    Implement triple exponential smoothing (aka Holt-Winters method)

--- anompy/detector/smoothing.py
+++ anompy/detector/smoothing.py
@@ -50,3 +50,73 @@ class DoubleExponentialSmoothing(BaseDetector):
             self.level, self.trend = self.level, x - self.level
 
         return (self.level + self.trend) > self.threshold
+
+
+class TripleExponentialSmoothing(BaseDetector):
+
+    def __init__(self, season_length=10, alpha=0.5, beta=0.5, gamma=0.5, threshold=0.):
+        self.season_length = season_length
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        self.threshold = threshold
+        self.series = []
+
+    def forecast(self):
+        assert len(self.series) >= self.season_length, 'at least one season should be observed'
+
+        if not hasattr(self, 'seasonals'):  # start creating forecast model
+            self.seasonals = self.initial_seasonal_components(self.series, self.season_length)
+            self.level = self.series[0]
+            self.trend = self.initial_trend(self.series, self.season_length)
+            for i in range(1, len(self.series)):
+                x = self.series[i]
+                component_index = i % self.season_length
+
+                last_level, self.level = self.level, self.alpha * (x - self.seasonals[component_index]) + (1. - self.alpha) * (self.level + self.trend)
+                self.trend = self.beta * (self.level - last_level) + (1. - self.beta) * self.trend
+                self.seasonals[component_index] = self.gamma * (x - self.level) + (1. - self.gamma) * self.seasonals[component_index]
+
+            self.forecast_count = 0
+
+        self.forecast_count += 1
+
+        forecast_component_index = (self.forecast_count + len(self.series) - 1) % self.season_length
+        return (self.level + self.forecast_count * self.trend) + self.seasonals[forecast_component_index]
+
+    def observe(self, x):
+        if hasattr(self, 'forecast_count'):  # already called `forecast()`, so we cannot update seasonality model
+            return self.forecast() > self.threshold
+        self.series.append(x)
+        return x > self.threshold
+
+    @staticmethod
+    def initial_trend(series, season_length):
+        accum_trend_average = 0.
+        for i in range(season_length):
+            # difference between neighbor seasons' i-th points (= `season_length` points from i to i+season_length)
+            trend_average = float(series[i + season_length] - series[i]) / season_length
+            accum_trend_average += trend_average
+        return accum_trend_average / season_length
+
+    @staticmethod
+    def initial_seasonal_components(series, season_length):
+        """Each seasonal point is estimated based on initial value of component.
+        """
+        n_seasons = int(len(series) / season_length)
+        season_averages = [0.] * n_seasons
+        for s in range(n_seasons):
+            head = season_length * s
+            tail = head + season_length
+            season_averages[s] = sum(series[head:tail]) / float(season_length)
+
+        components = [0.] * season_length
+        for i in range(season_length):
+            # sum of difference between each season's i-th point of series and average value of the corresponding season
+            accum_deviation = 0.
+            for s in range(n_seasons):
+                j = s * season_length + i
+                accum_deviation += (series[j] - season_averages[s])
+            components[i] = accum_deviation / n_seasons
+
+        return components
--- anompy/detector/tests/test_smoothing.py
+++ anompy/detector/tests/test_smoothing.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from anompy.detector.smoothing import ExponentialSmoothing, DoubleExponentialSmoothing
+from anompy.detector.smoothing import ExponentialSmoothing, DoubleExponentialSmoothing, TripleExponentialSmoothing
 
 
 class ExponentialSmoothingTestCase(TestCase):
@@ -30,4 +30,39 @@ class DoubleExponentialSmoothingTestCase(TestCase):
         for i in range(len(series)):
             anomaly = detector.observe(series[i])
             self.assertTrue(anomaly)
+            self.assertAlmostEqual(detector.forecast(), expected[i], delta=eps)
+
+
+class TripleExponentialSmoothingTestCase(TestCase):
+
+    def setUp(self):
+        self.series = [30, 21, 29, 31, 40, 48, 53, 47, 37, 39, 31, 29, 17, 9, 20, 24, 27, 35, 41, 38,
+                       27, 31, 27, 26, 21, 13, 21, 18, 33, 35, 40, 36, 22, 24, 21, 20, 17, 14, 17, 19,
+                       26, 29, 40, 31, 20, 24, 18, 26, 17, 9, 17, 21, 28, 32, 46, 33, 23, 28, 22, 27,
+                       18, 8, 17, 21, 31, 34, 44, 38, 31, 30, 26, 32]
+        self.slen = 12
+
+    def test_initial_trend(self):
+        initial_trend = TripleExponentialSmoothing.initial_trend(self.series, self.slen)
+        expected = -0.7847222222222222
+        self.assertAlmostEqual(initial_trend, expected, delta=1e-6)
+
+    def test_initial_seasonal_components(self):
+        components = TripleExponentialSmoothing.initial_seasonal_components(self.series, self.slen)
+        expected = [-7.4305555555555545, -15.097222222222221, -7.263888888888888, -5.097222222222222, 3.402777777777778, 8.069444444444445, 16.569444444444446, 9.736111111111112, -0.7638888888888887, 1.902777777777778, -3.263888888888889, -0.7638888888888887]
+        eps = 1e-6
+
+        for i in range(self.slen):
+            self.assertAlmostEqual(components[i], expected[i], delta=eps)
+
+    def test(self):
+        detector = TripleExponentialSmoothing(season_length=self.slen, alpha=0.716, beta=0.029, gamma=0.993)
+
+        for x in self.series:
+            self.assertTrue(detector.observe(x))
+
+        expected = [22.42511411230803, 15.343371755223066, 24.14282581581347, 27.02259921391996, 35.31139046245393, 38.999014669337356, 49.243283875692654, 40.84636009563803, 31.205180503707012, 32.96259980122959, 28.5164783238384, 32.30616336737171]
+        eps = 1e-6
+
+        for i in range(self.slen):
             self.assertAlmostEqual(detector.forecast(), expected[i], delta=eps)
